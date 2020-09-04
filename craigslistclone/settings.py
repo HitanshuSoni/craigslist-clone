2752d7fb4ff6357c4d44403d5757a609324974e1
Hitanshu Soni <hitanshusoni10@gmail.com>
2020-09-04 17:50:24 +0530

    Get Reay For Heroku

--- craigslistclone/settings.py
+++ craigslistclone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django.heroku.settings(locals())
